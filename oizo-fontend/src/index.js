9180940421a1832bd312aafa729bb0c4ccb8a276
Omar <54633759+OmarBENOTMANE@users.noreply.github.com>
2022-02-03 16:35:45 +0100

    update

--- oizo-fontend/src/index.js
+++ oizo-fontend/src/index.js
@@ -1,18 +1,18 @@
-import React, {Suspense} from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
-import './i18next';
+import "./i18next";
 
 ReactDOM.render(
-  <Suspense fallback={(<div>Loading...</div>)}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Suspense fallback={<div>Loading...</div>}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Suspense>,
-  document.getElementById('root')
+  document.getElementById("root"),
 );
 
 reportWebVitals();
